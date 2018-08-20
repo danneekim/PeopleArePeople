28f96c06edfada7b51f255983c9db0db6ba5e068
student_113 <student_113@ool-ad020267.dyn.optonline.net>
2018-08-20 11:31:44 -0400

    Created Schema

--- app-backend/db/schema.sql
+++ app-backend/db/schema.sql
@@ -1,15 +1,29 @@
-DROP TABLE IF EXISTS <TABLENAME1>;
-DROP TABLE IF EXISTS <TABLENAME2>;
+DROP TABLE IF EXISTS user_interest;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS interests;
 
 -- This is our schema where we would list our table column names/properties
 
-CREATE TABLE <TABLENAME1> (
-    -- tablename1_id: SERIAL PRIMARY KEY,
-    -- 
-)
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    cohort VARCHAR(255) NOT NULL,
+    horoscope VARCHAR(255) NOT NULL,
+    date_created TIMESTAMP NOT NULL DEFAULT NOW (),
+    password VARCHAR(255)
+);
 
-CREATE TABLE <TABLENAME2>(
-    -- tablename2_id: SERIAL PRIMARY KEY,
+CREATE TABLE interests (
+    id SERIAL PRIMARY KEY,
+    categories VARCHAR(64) NOT NULL,
+    interests VARCHAR(64) NOT NULL
+);
 
-)
+CREATE TABLE user_interest (
+    id SERIAL PRIMARY KEY,
+    users_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
+    interest_id INTEGER REFERENCES interests (id) ON DELETE CASCADE
+);
+
 
