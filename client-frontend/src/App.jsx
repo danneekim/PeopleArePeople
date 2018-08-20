26d931881beddee779bcdab36529c69431bcb92e
Elizabeth Fun <elizabethfun@gmail.com>
2018-08-20 12:21:25 -0400

    Elizabeth update

--- client-frontend/src/App.jsx
+++ client-frontend/src/App.jsx
@@ -5,52 +5,54 @@ import NewUser from './NewUser';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+ constructor(props) {
+   super(props);
 
-    this.state = {
-      currentView: 'SplashPage'
-    }
-  }
+   this.state = {
+     currentView: 'SplashPage'
+   }
 
-
-
-
+   this.handleLinkClick = this.handleLinkClick.bind(this);
+ }
 
 
 
 
 
-  determineWhichToRender() {
-    const { currentView } = this.state;
-    const { newUser } = this.state;
 
-    switch (currentView) {
-      case 'SplashPage':
-        return <SplashPage />;
-        break;
-      // case 'NewUser':
-      //   return <NewUser />;
-      //   break;
-    }
-  }
 
-  handleLinkClick(link) {
-    this.setState({currentView: link});
-  }
 
-  render() {
-    const links = [
-      'SplashPage',
-      // 'NewUser',
-    ];
 
-    return (
-      <div>
-        <Header links={links}/>
-      </div>
-    )
-  }
+ determineWhichToRender() {
+   const { currentView } = this.state;
+   // const { NewUser } = this.state;
+
+   switch (currentView) {
+     case 'SplashPage':
+       return <SplashPage />;
+     
+     case 'NewUser':
+       return <NewUser />;
+   }
+ }
+
+ handleLinkClick(links) {
+   this.setState({currentView: links});
+ }
+
+ render() {
+   const links = [
+     'NewUser',
+   ];
+
+   return (
+     <div>
+       <Header
+         onClick={this.handleLinkClick.bind(this)}
+         links={links}/>
+     </div>
+   )
+ }
 }
 
 export default App;
