d92aafcdbc9acfc2e7710eb0de743519ce2d196e
Elizabeth Fun <elizabethfun@gmail.com>
2018-08-23 09:25:37 -0400

    fixed bugs with 500 server error, created no match message and also reset the arry after we toggle to the next category tab

--- client-frontend/src/App.jsx
+++ client-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import NewUser from './NewUser';
 import UserIndex from './UserIndex';
 import FilterPage from './FilterPage';
 import Interests from './Interests';
+import 'bulma/css/bulma.css'
 import {
   fetchUsers,
   saveUser,
@@ -28,6 +29,9 @@ class App extends Component {
       checkedInterests: [],
       idToEdit: '',
       matches: [],
+      selectedCategory: null,
+      selectedInterests: null,
+      //we want to declare these variables because we're going to use them in the filterpage later
     }
     this.createUser = this.createUser.bind(this);
     this.callingInterests = this.callingInterests.bind(this);
@@ -39,21 +43,30 @@ class App extends Component {
 
   componentDidMount() {
     fetchUsers()
-    .then(data => this.setState({ users: data}))
+      .then(data => this.setState({ users: data }))
   }
 
   callingInterests(category) {
-    fetchInterestsByCategory(category) 
-        .then(data => this.setState({interests: data}));
+    fetchInterestsByCategory(category)
+      .then(data => this.setState({
+        interests: data,
+        selectedCategory: category,
+        selectedInterests: null, //keep track of knowing whether or not to use the emptymessage
+        matches: [], //we need it to be empty(reset) so we can get rid of the emptymessage
+      })
+    );
   }
 
-  callingMatches(id) {
-    fetchUsersByInterest(id)
-      .then(data => this.setState({matches: data}))
+  callingMatches(interestId) {
+    fetchUsersByInterest(interestId)
+      .then(data => this.setState({ 
+        matches: data,
+        selectedInterests: interestId,
+      }));
   }
 
 
-  setIdToEdit(id){
+  setIdToEdit(id) {
     this.setState({
       idToEdit: id,
       currentView: "EditUserInfo",
@@ -64,29 +77,29 @@ class App extends Component {
   createUser(user) {
     console.log(user)
     saveUser(user)
-    .then(data => fetchUsers())
-    .then(data => {
-      this.setState({
-        currentView: "Interests",
-        users: data,
+      .then(data => fetchUsers())
+      .then(data => {
+        this.setState({
+          currentView: "Interests",
+          users: data,
+        })
       })
-    })
-    .catch(e => {
-      console.log(e);
-    })
+      .catch(e => {
+        console.log(e);
+      })
   }
 
   createInterests(final) {
     saveInterests(final)
-    .then(data => fetchInterests())
-    .then(data => {
-      this.setState({
-        checkedInterests: data
+      .then(data => fetchInterests())
+      .then(data => {
+        this.setState({
+          checkedInterests: data
+        })
       })
-    })
-    .catch(e => {
-      console.log(e);
-    })
+      .catch(e => {
+        console.log(e);
+      })
   }
 
 
@@ -103,31 +116,33 @@ class App extends Component {
           onClick={this.handleLinkClick.bind(this)}
         />;
       case 'FilterPage':
-        return <FilterPage 
-                callingInterests={this.callingInterests}
-                interests={this.state.interests} 
-                callingMatches={this.callingMatches}
-                matches={this.state.matches}
-               />;
+        return <FilterPage
+          callingInterests={this.callingInterests}
+          interests={this.state.interests}
+          callingMatches={this.callingMatches}
+          matches={this.state.matches}
+          selectedCategory={this.state.selectedCategory}
+          selectedInterests={this.state.selectedInterests}
+        />;
       case 'UserIndex':
-        return <UserIndex 
-                  users={this.state.users} 
-                  setIdToEdit={this.setIdToEdit}
-                />;
+        return <UserIndex
+          users={this.state.users}
+          setIdToEdit={this.setIdToEdit}
+        />;
       case 'Interests':
-        return <Interests 
-                users={this.state.users}
-                interests={this.state.interests} 
-                callingInterests={this.callingInterests}
-                onSubmit={this.createInterests}
-                />; //don't need to put state because it's a function
+        return <Interests
+          users={this.state.users}
+          interests={this.state.interests}
+          callingInterests={this.callingInterests}
+          onSubmit={this.createInterests}
+        />; //don't need to put state because it's a function
       case 'EditUserInfo':
-        return <EditUserInfo    
-            users={this.state.users}
-            idToEdit={this.state.idToEdit}
+        return <EditUserInfo
+          users={this.state.users}
+          idToEdit={this.state.idToEdit}
         />
       default:
-      }
+    }
 
   }
 
