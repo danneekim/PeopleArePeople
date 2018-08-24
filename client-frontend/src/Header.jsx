86c879aad4893e61da6ccb04c4b81134ce5d1d4b
Elizabeth Fun <elizabethfun@gmail.com>
2018-08-24 00:53:09 -0400

    added logo

--- client-frontend/src/Header.jsx
+++ client-frontend/src/Header.jsx
@@ -2,21 +2,31 @@ import React from 'react';
 
 function Header(props) {
     const { links } = props;
-    return(
+    return (
         <div>
-        <div className = "parentContainer">
-            {links.map(link => {
-                return<div
-                    key={link}
-                    onClick={()=> props.onClick(link)}
-                >{link}
-                
-                </div>
-            })
-            }
-            
-        </div> 
-        </div>    
+
+
+         <div className="parentContainer">
+            <div className="logo">    
+                <ul>
+                <li>people</li>
+                <li className="bigAre">are</li>
+                <li>people</li>
+                </ul>
+            </div>
+
+                {links.map(link => {
+                    return <div
+                        key={link}
+                        onClick={() => props.onClick(link)}
+                    >{link}
+
+                    </div>
+                })
+                }
+
+            </div>
+        </div>
     )
 }
 
